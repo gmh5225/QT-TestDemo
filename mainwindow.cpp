708807e8498cf09fab2af3e3e8f58683034f2bec
gmh5225 <2315157@qq.com>
2022-07-01 15:33:56 +0800

    Update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <Windows.h>
 #include <QMessageBox>
 
 int gv = 0;
@@ -45,4 +46,30 @@ void MainWindow::on_pushButton_5_clicked()
     int a = gv;
     QString qstr = QString("%1").arg(a, sizeof(int) * 2, 16, QChar('0').toUpper());
     QMessageBox::information(nullptr, qstr, qstr);
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    float x = 1.1;
+    float y = 1.2;
+    float z = x + y;
+    auto qstr = QString::number(z);
+    QMessageBox::information(nullptr, qstr, qstr);
+}
+
+LONG NTAPI VehExceptionHandler(EXCEPTION_POINTERS *ExceptionInfo)
+{
+    //......
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    //install veh
+    auto ptr = AddVectoredExceptionHandler(TRUE, VehExceptionHandler);
+    if (ptr) {
+        QMessageBox::information(nullptr, "veh ok", "veh ok");
+    } else {
+        QMessageBox::information(nullptr, "veh failed", "veh failed");
+    }
 }
